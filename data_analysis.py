016994beb2d1680b501b554ff4bef0f59dceca4e
KarloSlot <k.h.r.slot@student.rug.nl>
2022-09-22 15:52:01 +0200

    Add function to calculate average length in words

--- data_analysis.py
+++ data_analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import statistics as stat
+from nltk.tokenize import RegexpTokenizer
 
 
 def pprint_label_distribution(label_distribution):
@@ -11,9 +13,17 @@ def pprint_label_distribution(label_distribution):
         print('\"{0}\": {1} ({2})'.format(label, count, pct))
 
 
-def get_label_distribution(df, label):
+def get_label_distribution(df, column):
     """Return a Series containing counts of a label in a DataFrame column"""
-    return df[label].value_counts()
+    return df[column].value_counts()
+
+def get_avg_length(df, column):
+    tokenizer = RegexpTokenizer(r'\w+')
+    texts = df[column].to_list()
+    text_lengths = [len(tokenizer.tokenize(text)) for text in texts]
+    avg_length = stat.mean(text_lengths)
+    return avg_length
+
 
 
 def main():
@@ -24,6 +34,10 @@ def main():
     # Get label distribution of the column 'label_sexist' in training data
     label_dist_a = get_label_distribution(train_df, 'label_sexist')
     pprint_label_distribution(label_dist_a)
+
+    # Average length of texts in training data in words
+    length_w_a = get_avg_length(train_df, 'text')
+    print(length_w_a)
 
 
 if __name__ == "__main__":
